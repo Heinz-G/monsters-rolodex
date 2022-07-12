54595880ecf599e19ed926be63ff3c23c782c296
CryptoElit <heinzgericke@outlook.com>
2022-07-12 11:23:09 +0200

    setState and Shallow Merge nr30

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Heinz'
+      name: {firstName: 'Heinz', lastName: 'Gericke'},
+      company: 'VoidWink'
     }
   }
   render() {
@@ -16,10 +17,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
+          <p>
+          Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}.
+          </p>
           <button 
           onClick={() => {
-            this.setState({name: 'Joe'});
+            this.setState({name: {firstName:'Joe', lastName: 'Hart'}});
+            console.log(this.state);
             }}
           >
           Change Name</button>
