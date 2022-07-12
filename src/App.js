4458d7797f590b6b303d30162a0d0bd7cc8894f9
CryptoElit <heinzgericke@outlook.com>
2022-07-12 12:09:27 +0200

    mapping arrays to elements nr32

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
@@ -8,34 +8,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: 'Heinz', lastName: 'Gericke'},
-      company: 'VoidWink'
+      monsters: [
+       {
+          name: 'Linda'
+        },
+       {
+          name: 'Frank'
+        },
+       {
+          name: 'Jacky'
+        },
+        {
+          name: 'Heinz'
+        },
+      ]
+      
     }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}.
-          </p>
-          <button 
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: {firstName: 'Joe', lastName: 'Hart'},
-                  };
-                }, 
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-          Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+          })
+        }
       </div>
     );
   }
