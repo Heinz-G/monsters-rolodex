a54c8ad6d03e12dad69a2e764e58cf77a85a7834
CryptoElit <heinzgericke@outlook.com>
2022-07-14 00:31:23 +0200

    searching and filtering

--- src/App.js
+++ src/App.js
@@ -33,22 +33,28 @@ class App extends Component {
     console.log('render');
     return (
       <div className='App'>
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
-          {this.state.monsters.filter((monster  ) => {
-            return(
-              <div key={monster.id}>
-              </div>
-            );
-          }
-          )}
+        <input className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={(event) => {
           console.log(event.target.value);
-        }} />
+          const searchString = event.target.value.toLocaleLowerCase();
+          const filteredMonsters = this.state.monsters.filter((moster) => {
+            return moster.name.toLocaleLowerCase.includes(searchString);
+          });
+
+          this.setState(() => {
+            return {monsters: filteredMonsters};
+          })
+        }} 
+
+      />
         
-        {this.state.monsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-              </div>
+      {this.state.monsters.map((monster) => {
+        return (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
             );
           })
         }
